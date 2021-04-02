4d5151c142bcaa3ccfc24df7c013b4bd7d1bc571
eduard <edik.tv@mail.ru>
2021-04-02 15:42:37 +0600

    added function setClick

--- table.js
+++ table.js
@@ -9,6 +9,7 @@ let data=[];
 let SplitedData=[];
 //разделяю массив
 (function splitedData(){
+    //генерирую массив с рядом в 7 элементов 
     let RowIndex=-1;
     for(let i =0; i<data.length;i++){
         
@@ -28,28 +29,47 @@ let SplitedData=[];
 
 let bodyTable=document.querySelector('#tbody')
 for(let TimeRows =0;TimeRows<24;TimeRows++){
+    //добавляю ряд в таблицу
     let rowElement = document.createElement('tr')
     bodyTable.appendChild(rowElement)
     for(let timeCellls=0;timeCellls<7;timeCellls++){
+        //добавляю строку и аттрибутты по которым я буду выбитать элемент
         let timeCell = document.createElement('td');
         timeCell.setAttribute('data-array-row',TimeRows);
         timeCell.setAttribute('data-array-item',timeCellls);
+        //события для изменения значения и цвета 
         timeCell.addEventListener('click',chnageColorOnClick);
         timeCell.addEventListener('mouseover',chnageColorOnMouseOver);
-
+        //добавляю модифицированные элементы 
         timeCell.innerText=SplitedData[TimeRows][timeCellls];
         bodyTable.children[TimeRows].appendChild(timeCell);
     }
 }
+let isMouseClicked=false;
+//проверяю зажата ли кнопка мыши
+
+function setMouseClick(boolValue){
+        console.log(isMouseClicked)
+       return isMouseClicked=boolValue
+       
+}
+
+ document.querySelector('table').addEventListener('mouseup',()=>{setMouseClick(false)})
+ document.querySelector('table').addEventListener('mousedown',()=>{setMouseClick(true)})
 
 function chnageColorOnMouseOver(e){
+    //получаю дата-аттрибуты при клике на элемент 
     let firstAttribute=e.target.attributes[0].nodeValue;
     let secondAttribute=e.target.attributes[1].nodeValue;
-    SplitedData[firstAttribute][secondAttribute]=5;
-    e.target.innerText=5
+    //закрашиваю значения если мышь зажата
+    if(isMouseClicked===true){
+        SplitedData[firstAttribute][secondAttribute]=5;
+        e.target.innerText=5
+    }
 }
 
 function chnageColorOnClick(e){
+    //получаю дата-аттрибуты при наведении  на элемент 
     let firstAttribute=e.target.attributes[0].nodeValue;
     let secondAttribute=e.target.attributes[1].nodeValue;
     SplitedData[firstAttribute][secondAttribute]=5;
